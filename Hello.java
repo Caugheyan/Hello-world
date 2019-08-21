d13cf6ba0a03833cc3a91c037d79b64a27ebfeb9
Caugheyan <54364962+Caugheyan@users.noreply.github.com>
2019-08-21 10:19:51 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/************************************************************************
+* Hello
+*************************************************************************
+* first class
+*________________________________________________________________________
+* Alycia Caughey
+* August 21, 2019
+* CMSC 255 section 3 
+*************************************************************************/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Alycia Caughey");
+		System.out.println("Hello");
+		System.out.println("first class");
+		System.out.println("8/21/2019");
+		System.out.println("CMSC 255 section 3");
+		System.out.println();
+	}
+
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
